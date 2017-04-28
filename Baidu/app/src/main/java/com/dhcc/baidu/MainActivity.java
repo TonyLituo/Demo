91af43d43e0b994959cb08f865ad29f8195dc9d7
TonyLituo <143980456@qq.com>
2017-04-28 22:23:49 +0800

    网页代码

--- Baidu/app/src/main/java/com/dhcc/baidu/MainActivity.java
+++ Baidu/app/src/main/java/com/dhcc/baidu/MainActivity.java
@@ -3,8 +3,11 @@ package com.dhcc.baidu;
 import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     Button mButton;
     Button mButton1;
     TextView mTextView;
+    EditText mEditText;
+
     private static final int LOCATION_CODE_SDCARD = 2;
 
     @Override
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*占位符*/
+        mEditText = (EditText) this.findViewById(R.id.edt);
         mTextView = (TextView) this.findViewById(R.id.tv);
         String data = getResources().getString(R.string.data);
         data = String.format(data, 100, 10.3, "2017-07-01");
@@ -58,6 +64,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mLocationClient.stop();
+            }
+        });
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (null != s) {
+
+                }
             }
         });
     }
